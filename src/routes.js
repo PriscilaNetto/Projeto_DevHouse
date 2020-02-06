06490e4e531748f32c314daa08ee876a8ad67daa
Priscila Netto <priscila.nnetto@hotmail.com>
2020-02-06 15:41:36 -0300

    inclusão import SessionController

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import {Router} from 'express';
 
+import SessionController from './controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.status(200).send("Oi mundo!")
-});
+routes.post('/sessions', SessionController.store);
 
 export default routes;
