d4747ce2ff61e2ddd03dd0315dd8c45ecce4da46
Priscila Netto <priscila.nnetto@hotmail.com>
2020-02-06 16:10:49 -0300

    add HouseController c req testada

---
+++ src/controllers/HouseController.js
@@ -0,0 +1,24 @@
+import User from '../models/House';
+
+class HouseController{
+
+  async store(req, res) {
+
+    return res.json({message: 'ok'})
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default new HouseController();
