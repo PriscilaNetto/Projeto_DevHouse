11734933fd81062a3f1a16f68593d75b2f4a5e91
Priscila Netto <priscila.nnetto@hotmail.com>
2020-02-06 15:00:31 -0300

    criação na class app conexão com o mongo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 class App{
 
   constructor(){
     this.server = express(); //this é referente a class App.
+    
+    mongoose.connect('mongodb+srv://devhouse:devhouse0212@devhouse-4qork.mongodb.net/devhouse?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  });
 
     this.middlewares();
     this.routes();
