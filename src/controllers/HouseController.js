c270e54df60ca1b51607aef7860679ace4453f5f
Priscila Netto <priscila.nnetto@hotmail.com>
2020-02-06 17:05:26 -0300

    teste de imagem e store

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -3,6 +3,11 @@ import User from '../models/House';
 class HouseController{
 
   async store(req, res) {
+    console.log(req.body);
+
+    console.log(req.file);
+    
+    
 
     return res.json({message: 'ok'})
   }
