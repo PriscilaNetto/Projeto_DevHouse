7fad00361117c3b1a70513458f685dca206f3d66
Priscila Netto <priscila.nnetto@hotmail.com>
2020-02-07 18:51:48 -0300

    criação do método update

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,6 +1,6 @@
 import User from '../models/House';
-
 import House from '../models/House';
+import User from '../models/User';
 
 class HouseController{
 
@@ -14,7 +14,7 @@ class HouseController{
    
   async store(req, res) {
     const { filename } = req.file;
-    const { description, price, location, status } = req.body;
+    const { description, price, address, status } = req.body;
     const { user_id } = req.headers;
 
     const house = await House.create ({
@@ -22,11 +22,39 @@ class HouseController{
       thumbnail: filename,
       description,
       price,
-      location,
+      address,
       status,
     });
   
     return res.json({house})
+  }
+
+  async update(req, res) {
+    const { filename } = req.file;
+    const { house_id } = req.params;
+    const { description, price, address, status } = req.body;
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+    const houses = await House.findById(house_id);
+    
+    if(String(user._id) !== String(houses.user)){
+      return res.status(401).send('Não autorizado!');
+    }
+
+
+
+    const houses = await House.updateOne({ _id: house_id },{
+      user: user_id,
+      thumbnail: filename,
+      description,
+      price,
+      address,
+      status,
+    })
+
+
+    return res.send('Dados alterados com sucesso!');
   }
 
 }
