64a7feadb74c2665bc5505befd7c301d45d874b2
Priscila Netto <priscila.nnetto@hotmail.com>
2020-02-07 12:03:50 -0300

    add url para acessar imagem

--- src/models/House.js
+++ src/models/House.js
@@ -11,6 +11,17 @@ const HouseSchema = new Schema ({
     type: Schema.Types.ObjectId, //para buscar pelo id qual é o usuário 
     ref: 'User'
   }
+}, {
+
+  toJSON: {
+    virtuals: true  //colocar a variável virtuals junto com a requisição quando ela for feita
+  }
+
+
+});
+
+HouseSchema.virtual('thumbnail_url').get(function(){
+  return `http://localhost:3333/files/${this.thumbnail}`; //criação de um campo virtual para acessar a url e não irá armazenar a imagem no banco
 });
 
 export default model('House', HouseSchema)
