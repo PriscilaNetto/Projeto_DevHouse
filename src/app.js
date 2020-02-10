a9308b8eebb6b0fbe4320c3877f29c5b7f50c15a
Priscila Netto <priscila.nnetto@hotmail.com>
2020-02-10 11:43:11 -0300

    importação do cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
@@ -17,6 +18,8 @@ class App{
     this.routes();
   }
   middlewares(){
+
+    this.server.use(cors());
 
     this.server.use(
       '/files',
